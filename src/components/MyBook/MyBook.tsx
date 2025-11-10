036b93aecbbadf6acf52e17cdb92e564fdfc9b59
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-11-10 20:13:37 +0100

    add scss for my reading content part

--- src/components/MyBook/MyBook.tsx
+++ src/components/MyBook/MyBook.tsx
@@ -10,19 +10,23 @@ const MyBook = () => {
 
 
     return <section className="primary">
-        <h2>My reading</h2>
-        <div>
+        <h2 className="contentTitle">My reading</h2>
+        <div className="readingContent">
+            <div className="readingWrap">
         <img
             src={book?.imageUrl}
             alt={`Book cover - ${book?.title}`}
             width="224"
             height="340"
+            className="readingImg"
             />
         <div>
-            <h4>{book?.title}</h4>
-            <p>{book?.author}</p>
+            <h4 className="readingTitle">{book?.title}</h4>
+            <p className="readingAuthor">{book?.author}</p>
         </div>
-            <img src={isReading? "/stop.svg" : "/play.svg"} width={50} height={50} alt={isReading? "Stop reading icon" : "Start reading icon"}/>
+
+            <img className="readingBtn" src={isReading? "/stop.svg" : "/play.svg"} width={50} height={50} alt={isReading? "Stop reading icon" : "Start reading icon"}/>
+            </div>
         </div>
 
     </section>
