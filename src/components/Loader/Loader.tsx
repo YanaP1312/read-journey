44992f5fcd1ef2b36d58c305c344757bb3e8b8d3
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-11-29 17:16:14 +0100

    add commen loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,5 +1,13 @@
+import {Triangle} from 'react-loader-spinner'
+
 const Loader = () => {
-  return <div>Loading...</div>;
+  return <div className='loader'><Triangle visible={true}
+  height="80"
+  width="80"
+  color="#4F92F7"
+  ariaLabel="triangle-loading"
+  wrapperStyle={{}}
+  wrapperClass=""/></div>;
 };
 
 export default Loader;
